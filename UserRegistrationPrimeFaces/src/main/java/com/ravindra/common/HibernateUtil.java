6dba50a892dc3aff5d942364dbfbce6ba7785858
Kakarla V S Ravindra Reddy <kvsravindrareddy@gmail.com>
2015-12-02 21:30:32 +0530

    test

--- UserRegistrationPrimeFaces/src/main/java/com/ravindra/common/HibernateUtil.java
+++ UserRegistrationPrimeFaces/src/main/java/com/ravindra/common/HibernateUtil.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class HibernateUtil {
+public class HibernateUtil{
 
 	private static final Logger logger = Logger.getLogger(HibernateUtil.class);
 
